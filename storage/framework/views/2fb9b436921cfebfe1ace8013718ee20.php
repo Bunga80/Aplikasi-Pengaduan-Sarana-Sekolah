bd9eabb64e1605df17f9f6eca8f5979310d51ed5
Bunga80 <bungameliaa438@gmail.com>
2026-03-05 21:21:02 +0700

    Delete storage/framework/views directory

--- storage/framework/views/2fb9b436921cfebfe1ace8013718ee20.php
+++
@@ -1,8 +0,0 @@
-
-<?php $__env->startSection('body'); ?>
-<?php echo $__env->make('layouts.navbar.siswa', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
-<div class="container">
-    <?php echo $__env->yieldContent('content'); ?>
-</div>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layouts.main', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\xampp\htdocs\app-apss-bunga\resources\views/layouts/siswa.blade.php ENDPATH**/ ?>
--- storage/framework/views/32a6cda894798214864de09c4a2eb7bb.php
+++
@@ -1,8 +0,0 @@
-
-<?php $__env->startSection('body'); ?>
-<?php echo $__env->make('layouts.navbar.admin', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
-<div class="container">
-    <?php echo $__env->yieldContent('content'); ?>
-</div>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layouts.main', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\xampp\htdocs\app-apss-bunga\resources\views/layouts/admin.blade.php ENDPATH**/ ?>
